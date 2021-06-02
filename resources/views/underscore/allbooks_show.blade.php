1d4490fb72b3e63d0dd834547bc53fd3dd210539
NJ <201810940@gordoncollege.edu.ph>
2021-06-02 10:03:16 +0800

    Initial Commit

---
+++ resources/views/underscore/allbooks_show.blade.php
@@ -0,0 +1,11 @@
+<tr>
+	<td><%= obj.book_id %></td>
+	<td><%= obj.title %></td>
+	<td><%= obj.author %></td>
+	<td><%= obj.description %></td>
+	<td><%= obj.category %>
+	</td>
+	<td><a class="btn btn-success"><%= obj.avaliable %></a></td>
+	<td><a class="btn btn-inverse"><%= obj.total_books %></a></td>
+	<td><a href="/editbook/<%= obj.book_id %>" class="btn btn-default">Edit</a></td>
+</tr>
